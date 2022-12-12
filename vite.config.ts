866245691c27d2ca760955f407f70b87de8be96a
82L <16747176+82L@users.noreply.github.com>
2022-12-12 08:41:21 +0100

    pwa intégration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [
+      svelte(),
+    VitePWA({ registerType: 'autoUpdate' })
+  ]
+})
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svelte()]
-})
+// export default defineConfig({
+//   plugins: [svelte()]
+// })
