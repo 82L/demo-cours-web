75ace1a09052b1416211e8950f1269d8e19d2c98
82L <16747176+82L@users.noreply.github.com>
2022-12-18 14:16:32 +0100

    Création SaveManger to save data on disk

---
+++ src/lib/SaveManager.svelte
@@ -0,0 +1,29 @@
+<script context="module" lang="ts">
+    const key = "dataGeoSaveApp"
+
+    export let data = []
+    export function LoadSavedData(){
+        let savedData = window.localStorage.getItem(key)
+        if(savedData != null){
+            data = JSON.parse(savedData)
+            data = data
+        }
+        console.log(data)
+    }
+
+    function SaveData(){
+        window.localStorage.setItem(key, JSON.stringify(data));
+    }
+
+    export function AddElementToData(position){
+        data.push({
+            dateKey: new Date().toJSON(),
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            deviceDirection: position.coords.heading
+        })
+        data = data;
+        SaveData();
+    }
+
+</script>
