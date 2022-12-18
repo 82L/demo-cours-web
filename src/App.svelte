9557ebd19059cad74cd9db0fc7fb4372102cd2de
82L <16747176+82L@users.noreply.github.com>
2022-12-18 14:20:36 +0100

    Implementation

--- src/App.svelte
+++ src/App.svelte
@@ -1,46 +1,28 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
-  import NestedFullScreen from './lib/NestedFullScreen.svelte'
+  import SaveManager,  { LoadSavedData, data }  from "./lib/SaveManager.svelte";
+  import CoordinateSavingButton from './lib/CoordinatesSavingButton.svelte';
+  import ImgFetcher from "./lib/ImgFetcher.svelte";
+
+  LoadSavedData()
+  let mapsHidden = true
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+  <button on:click={e => mapsHidden= !mapsHidden}>Toggle Maps</button>
+  {#if mapsHidden}
+    <CoordinateSavingButton/>
+  {:else }
+    <div class="maps-container">
+    {#each data as element}
+      <ImgFetcher imgData={element} />
+    {/each}
+    </div>
+  {/if}
 
-  <div class="card">
-    <Counter />
-  </div>
-  <NestedFullScreen/>
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+  .maps-container{
+    position: relative;
   }
 </style>
